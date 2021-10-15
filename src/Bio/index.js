2c658e7968fb864204645105639ac3af8ec62e66
Jae Doherty <jaeld97@gmail.com>
2021-10-15 10:38:23 -0700

    added bio page

---
+++ src/Bio/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Bio() {
+    return(
+        <div className="tab-content">
+            Bio Page
+            <p></p>
+        </div>
+    )
+}
+
+export default Bio;
